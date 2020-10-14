291ab67c75628abffdd20bda44413ccf34ca1061
deepio <alex.daigle2@gmail.com>
2020-10-14 13:44:46 -0400

    Test by comparing toml version versus library version

--- tests/test_kingghidorah.py
+++ tests/test_kingghidorah.py
@@ -1,5 +1,12 @@
+import pathlib
+import re
+
 from kingghidorah import __version__
 
 
 def test_version():
-  assert __version__ == "0.1.0"
+  # Get current file, parent, parent (AKA from Root KD Dir) and get the file "pyproject.toml"
+  with open(pathlib.Path(__file__).parent.parent / "pyproject.toml") as f:
+    pattern = f"(?<=version = \")(.*?)(?=\")"
+    release = re.findall(pattern, f.read(), re.DOTALL)[0]
+    assert __version__ == release
