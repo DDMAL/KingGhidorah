278ca86bef7bf8a60b7f7e07773c405865b840a3
deepio <alex.daigle2@gmail.com>
2020-10-14 14:32:33 -0400

    Added: Test for GetAllProjects regular and by name

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,3 +1,5 @@
+import re
+import string
 import unittest
 
 import kingghidorah as kd
@@ -8,13 +10,32 @@ from kingghidorah.utils import (
 
 
 class ProjectTests(unittest.TestCase):
+  def setUp(self):
+    # self.name = random_gen().replace("/", "_")
+    self.name = re.sub(f"[{string.punctuation}]", "", random_gen())
 
   def test_create_project(self):
-    name = random_gen()
+    name = self.name
     with create_project(name=name) as p:
       assert p["name"] == name
 
   def test_delete_project(self):
-    name = random_gen()
+    name = self.name
     with create_project(name=name) as p:
       assert "delete" in kd.DeleteProject(uuid=p["uuid"])["status"]
+  
+  def test_get_project(self):
+    name = self.name
+
+    with create_project(name=name) as p:
+      assert isinstance(kd.GetAllProjects(), list)
+
+      try:
+        tmp = kd.GetAllProjects(name=p["name"])[0]
+      except:
+        raise Exception(name)
+
+      try:
+        assert tmp["name"] == name
+      except:
+        raise Exception("\n" + tmp["name"] + "\n\n" + name)
